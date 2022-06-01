8fd39921e7392845edf96615fcbcef8461216d2d
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-06-01 20:19:49 -0300

    adiciona o enunciado do exercicio 01 como comentario

--- ex01.js
+++ ex01.js
@@ -1,6 +1,15 @@
 const prompt = require("prompt-sync")();
 console.clear();
 
+/* 
+Peça ao usuário uma quantidade de linhas e e outra de colunas, 
+e imprima essas informações na forma de uma tabela como no exemplo de 3 linhas e 4 colunas:
+[ 0, 1, 2, 3 ]
+[ 1, 2, 3, 4 ]
+[ 2, 3, 4, 5 ]
+
+*/
+
 let linhas = +prompt("Digite a quantidade de linhas: ");
 let colunas = +prompt("Digite a quantidade de colunas: ");
 
@@ -12,7 +21,6 @@ for (let i = 0; i < linhas; i++) {
     arrayNovo[i][j] = "";
   }
 }
-console.log(arrayNovo);
 let element = 0;
 
 for (let l = 0; l < linhas; l++) {
@@ -36,4 +44,3 @@ for (let L = 0; L < linhas; L++) {
 }
 
 console.log(valoresArray);
-console.log(arrayNovo);
